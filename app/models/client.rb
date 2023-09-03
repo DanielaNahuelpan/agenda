734e5524e23f06dd75a414bb085eb832107f1673
Daniela <daniela.paz.n.s@gmail.com>
2023-09-03 16:29:40 -0300

    Agrego validación a los campos en el modelo client

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,9 @@ class Client < ApplicationRecord
     has_many :calls, dependent: :destroy
     has_many :users, through: :calls
     has_many :reasons, through: :calls
+    validates :name, presence: true
+    validates :phone, presence: true
+    validates :email, presence: true
 
     # Define una asociación a sí mismo llamada "client" (esto puede ser inusual).
         belongs_to :client, class_name: 'Client', optional: true
